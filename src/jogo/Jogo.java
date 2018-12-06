e6e37cbd44a8dcfe7f116fa27ab165d4f0cb3db6
Vinhal <lucasvp@algartelecom.com.br>
2018-12-06 21:40:57 -0200

    alterações na estilização

---
+++ src/jogo/Jogo.java
@@ -0,0 +1,79 @@
+package jogo;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
+
+public class Jogo extends JFrame {
+
+	/**
+	 * Serial gerado automaticamente apenas para efeito de serialização
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Jogo() {
+		construirMenuBar();
+		construirFase();
+		configurarTela();
+	}
+
+	public static void main(String[] args) {
+		new Jogo();
+	}
+
+	private JMenuBar construirMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.WHITE);
+		menuBar.setBorder(new LineBorder(Color.red));
+		JMenu menu = new JMenu("Opções");
+		JMenuItem sobre = new JMenuItem("Sobre");
+		sobre.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,"");
+			}
+		});
+
+		JMenuItem sair = new JMenuItem("Sair");
+		sair.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+
+			}
+		});
+
+		menu.add(sobre);
+		menu.add(new JSeparator());
+		menu.add(sair);
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
+
+		return menuBar;
+	}
+
+	private JPanel construirFase() {
+		Fase fase = new Fase();
+		add(fase);
+		return fase;
+	}
+
+	private void configurarTela() {
+		setSize(500, 420);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setTitle("AlienTrix");
+	}
+}
+
